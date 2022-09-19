c168279e00d21ef66736c124dbd474e6bf022de7
jide <jide.fr@gmail.com>
2022-09-19 13:51:23 +0200

    yo

--- src/App.js
+++ src/App.js
@@ -12,89 +12,119 @@ const combinations = [
 function App() {
   const ref = doc(db, "state", "app");
 
+  function navigate(url) {
+    updateDoc(ref, {
+      navigate: url,
+    });
+  }
+
   return (
     <div className="App">
-      <button
-        onClick={() =>
-          updateDoc(ref, {
-            reset: true,
-          })
-        }
-      >
-        RESET
-      </button>
-      <button
-        onClick={() =>
-          updateDoc(ref, {
-            doSubmitIntroAccess: "REPVP",
-          })
-        }
-      >
-        INTRO
-      </button>
-      <button
-        onClick={() =>
-          updateDoc(ref, {
-            doSubmitIAFlux: "ADBEC",
-          })
-        }
-      >
-        FLUX
-      </button>
-      <button
-        onClick={() =>
-          updateDoc(ref, {
-            doSubmitIALearning: combinations[0],
-          })
-        }
-      >
-        IA CARTOUCHE
-      </button>
-      <button
-        onClick={() =>
-          updateDoc(ref, {
-            doSubmitIALearning: combinations[1],
-          })
-        }
-      >
-        IA CANNETTE
-      </button>
-      <button
-        onClick={() =>
-          updateDoc(ref, {
-            doSubmitIALearning: combinations[2],
-          })
-        }
-      >
-        IA FEUILLE
-      </button>
-      <button
-        onClick={() =>
-          updateDoc(ref, {
-            doSubmitIALearning: combinations[3],
-          })
-        }
-      >
-        IA PHONE
-      </button>
-      <button
-        onClick={() =>
-          updateDoc(ref, {
-            doSubmitIALearning: combinations[4],
-          })
-        }
-      >
-        IA ACID
-      </button>
-      <button
-        onClick={() =>
-          updateDoc(ref, {
-            doSubmitIALearning: "RESPONSABLE",
-          })
-        }
-      >
-        CONSEQUENCES
-      </button>
+      <div>
+        <button onClick={() => navigate("/intro")}>/intro</button>
+        <button onClick={() => navigate("/intro/access")}>/intro/access</button>
+        <button onClick={() => navigate("/ia")}>/ia</button>
+        <button onClick={() => navigate("/ia/flux")}>/ia/flux</button>
+        <button onClick={() => navigate("/ia/learning")}>/ia/learning</button>
+        <button onClick={() => navigate("/ia/consequences")}>
+          /ia/consequences
+        </button>
+        <button onClick={() => navigate("/ia/consequences/video")}>
+          /ia/consequences/video
+        </button>
+        <button onClick={() => navigate("/emergency")}>/emergency</button>
+        <button onClick={() => navigate("/emergency/voice")}>
+          /emergency/voice
+        </button>
+        <button onClick={() => navigate("/emergency/timer")}>
+          /emergency/timer
+        </button>
+        <button onClick={() => navigate("/reset/win")}>/reset/win</button>
+        <button onClick={() => navigate("/reset/loose")}>/reset/loose</button>
+      </div>
+      <div>
+        <button
+          onClick={() =>
+            updateDoc(ref, {
+              reset: true,
+            })
+          }
+        >
+          RESET
+        </button>
+        <button
+          onClick={() =>
+            updateDoc(ref, {
+              doSubmitIntroAccess: "REPVP",
+            })
+          }
+        >
+          INTRO
+        </button>
+        <button
+          onClick={() =>
+            updateDoc(ref, {
+              doSubmitIAFlux: "ADBEC",
+            })
+          }
+        >
+          FLUX
+        </button>
+        <button
+          onClick={() =>
+            updateDoc(ref, {
+              doSubmitIALearning: combinations[0],
+            })
+          }
+        >
+          IA CARTOUCHE
+        </button>
+        <button
+          onClick={() =>
+            updateDoc(ref, {
+              doSubmitIALearning: combinations[1],
+            })
+          }
+        >
+          IA CANNETTE
+        </button>
+        <button
+          onClick={() =>
+            updateDoc(ref, {
+              doSubmitIALearning: combinations[2],
+            })
+          }
+        >
+          IA FEUILLE
+        </button>
+        <button
+          onClick={() =>
+            updateDoc(ref, {
+              doSubmitIALearning: combinations[3],
+            })
+          }
+        >
+          IA PHONE
+        </button>
+        <button
+          onClick={() =>
+            updateDoc(ref, {
+              doSubmitIALearning: combinations[4],
+            })
+          }
+        >
+          IA ACID
+        </button>
+        <button
+          onClick={() =>
+            updateDoc(ref, {
+              doSubmitIALearning: "RESPONSABLE",
+            })
+          }
+        >
+          CONSEQUENCES
+        </button>
+      </div>
     </div>
   );
 }
